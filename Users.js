2d79d52b0be6f5f7be92f512ac18355c172b240b
farteaga8912 <18072082+frarteaga@users.noreply.github.com>
2019-10-06 08:09:00 -0400

    mock users

---
+++ Users.js
@@ -0,0 +1,16 @@
+const Users = [
+    {
+        id: 1,
+        name: "john smith",
+        email: "john@yahoo.com",
+        department: "marketing"
+    },
+    {
+        id: 2,
+        name:  "emma lin",
+        email: "emma@yahoo.com",
+        department: "sales"
+    }
+];
+
+module.exports = Users;
